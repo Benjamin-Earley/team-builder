f62475d2929ecf7c6fb074c65eb9c019cafe6dc5
Benjamin Earley <benjaminearley@MacBook-Pro.local>
2020-03-19 21:16:36 -0400

    Added MVP inputs to form. Form submits and clears on submit. App renders Form component. Must store submitted data to state for on demand rendering.

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -2,39 +2,44 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [firstName, setFirstName] = useState('')
-  const [lastName, setLastName] = useState('')
-  const onSubmit = evt => {
-    evt.preventDefault()
+  const [user, setUser] = useState({firstName: '', lastName: ''})
+  const handleChange = evt => {
+    setUser({ ...user, [evt.target.name]: evt.target.value})
   }
-  const onfirstNameChange = evt => setFirstName(evt.target.value)
-  const onlastNameChange = evt => setLastName(evt.target.value)
+  const handleSubmit = evt => {
+    evt.preventDefault()
+    setUser({ firstName: '', lastName: ''})
+    // console.log(user.firstName, user.lastName)
+  }
   return (
     <div className="App">
-      <form onSubmit={evt => {
-        evt.preventDefault()
-      }}>
+      {console.log(user)}
+      <form onSubmit={evt => {handleSubmit(evt)}}>
         <label>
-          First Name: <input onChange={onfirstNameChange}
+          First Name: 
+          <input onChange={event => handleChange(event)}
           maxLength='20'
           placeholder='First Name'
           id='firstNameInput'
           name='firstName'
+          value={user.firstName}
           type='text'
           />
         </label>
         <label>
-          Last Name: <input onChange={onlastNameChange}
+          Last Name: 
+          <input onChange={event => handleChange(event)}
           maxLength='20'
           placeholder='Last Name'
           id='lastNameInput'
           name='lastName'
+          value={user.lastName}
           type='text'
           />
         </label>
         <button>Submit</button>
       </form>
-      <h5>Welcome to the team, {firstName} {lastName}!</h5>
+      <h5>Welcome to the team, {user.firstName} {user.lastName}!</h5>
     </div>
   );
 }
